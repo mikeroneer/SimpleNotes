cc4a2c4aab6e0e241f9dab4302c44a23ead7fe81
Michael Rockenschaub <rockenschaub.michael@gmail.com>
2016-06-26 19:02:57 +0200

    implemented textbox for tendant id in settings-page

--- SimpleNotes/ViewModels/SettingsViewModel.cs
+++ SimpleNotes/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using GalaSoft.MvvmLight;
-using SimpleNotes.Models;
+﻿using GalaSoft.MvvmLight;
 using SimpleNotes.Services;
 
 namespace SimpleNotes.ViewModels
@@ -12,24 +10,26 @@ namespace SimpleNotes.ViewModels
 
 		public int NumberOfShownNotes { get; set; } = 5;
 		public bool IsSortAscending { get; set; }
-
-		public string TenantId => "s1510237031";
+		public string TenantId { get; set; }
 
 		public SettingsViewModel(IStorageService storageService)
 		{
 			this.storageService = storageService;
+			LoadFromStorage();
 		}
 
 		public void SaveToStorage()
 		{
 			storageService.Write(nameof(NumberOfShownNotes), NumberOfShownNotes);
 			storageService.Write(nameof(IsSortAscending), IsSortAscending);
+			storageService.Write(nameof(TenantId), TenantId);
 		}
 
 		public void LoadFromStorage()
 		{
 			NumberOfShownNotes = storageService.Read<int>(nameof(NumberOfShownNotes), 5);
 			IsSortAscending = storageService.Read<bool>(nameof(IsSortAscending), false);
+			TenantId = storageService.Read<string>(nameof(TenantId), "s1510237031");
 		}
 	}
 }
